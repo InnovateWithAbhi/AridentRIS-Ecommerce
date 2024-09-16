adae5e035813c2f95d49e386ab00a1c13ca6e4c5
AbhishekHPatil <patilabhishek579@outlook.com>
2024-09-16 13:46:15 +0530

    navbar design finalize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js}"];
 export const theme = {
-  extend: {},
+  extend: {
+    fontFamily: {
+      sans: ["Roboto Condensed", "sans-serif"],
+    },
+  },
 };
 export const plugins = [];
