40574c5bf52ca3f67ad4d22c438298e779dc2540
AbhishekHPatil <patilabhishek579@outlook.com>
2024-09-16 17:31:11 +0530

    Path changed

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,31 +1,10 @@
-import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {
-  @Input() product: any;
-
-  products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      description: 'Description 1',
-      price: 100,
-      imageUrl: 'path/to/image1.jpg',
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      description: 'Description 2',
-      price: 200,
-      imageUrl: 'path/to/image2.jpg',
-    },
-    // Add more products as needed
-  ];
-}
+export class ProductListComponent {}
